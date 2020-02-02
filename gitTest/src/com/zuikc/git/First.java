3346732d3846e619b7db64cd01adb462330363da
sun_glb <HelloGit_glb@zuikc.com>
2020-02-03 02:27:53 +0800

    second commit First.java

--- gitTest/src/com/zuikc/git/First.java
+++ gitTest/src/com/zuikc/git/First.java
@@ -14,5 +14,7 @@ public class First {
         System.out.println("test git ...");
         System.out.println("test git ...");
         System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
     }
 }
