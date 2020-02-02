418807e3c7d76694fd46393de006d809ee6b4904
sun_glb <HelloGit_glb@zuikc.com>
2020-02-03 03:20:04 +0800

    edit by clone

--- gitTest/src/com/zuikc/First.java
+++ gitTest/src/com/zuikc/First.java
@@ -17,7 +17,7 @@ public class First {
         System.out.println("test git ...");
         System.out.println("test git ...");
         System.out.println("test git ...");
-        System.out.println("test git ...add by test");
+        System.out.println("test git ...edit by clone");
         System.out.println("test git add by clone...");
     }
 
