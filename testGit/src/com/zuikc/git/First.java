32b49ed0ea7777a70229a4e949ccfe820933e14b
sun_glb <HelloGit_glb@zuikc.com>
2020-02-03 01:34:10 +0800

    first commit First.java

---
+++ testGit/src/com/zuikc/git/First.java
@@ -0,0 +1,18 @@
+package com.zuikc.git;
+
+/**
+ * @program: testGit
+ * @description:
+ * @author: Sun
+ * @create: 2020/02/03 01:32
+ * @version: 1.0
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+    }
+}
