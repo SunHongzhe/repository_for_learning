8b76315de6c2fd809f2ca38f562a1af97dc41bf1
sun_glb <HelloGit_glb@zuikc.com>
2020-02-03 03:00:12 +0800

    first commit First.java

---
+++ gitTest/src/com/zuikc/First.java
@@ -0,0 +1,26 @@
+package com.zuikc;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @author: Sun
+ * @create: 2020/02/03 02:57
+ * @version: 1.0
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+        System.out.println("test git ...");
+    }
+
+    public void test() {
+        int i = 0;
+        for (int j = 0; j < 10; j++) {
+            i++;
+        }
+        System.out.println(i);
+    }
+
+}
